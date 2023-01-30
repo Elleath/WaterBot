0d9fa46a95e7e98e6943d037682e92b574ff8dc4
Elleath <kuzminykh.kirill@gmail.com>
2023-01-30 20:27:57 +0500

    Update tara_script1.py

--- tara/tara_scripts/tara_script1.py
+++ tara/tara_scripts/tara_script1.py
@@ -20,12 +20,13 @@ df = pd.read_csv("C:/Users/Kirill/Desktop/project/tara/tara_output.csv", delimit
 browser.get("https://voda174.ru")
 time.sleep(2)
 browser.execute_script("window.scrollTo(0, 900)")
-time.sleep(1)
+time.sleep(2)
 
-button1 = browser.find_element("xpath", '//*[@id="5f97f35effcd48f0bc1d8416f1e6f684"]/div/div/a')
+button1 = browser.find_element("xpath", '//*[@id="06b25ff20b6142b6b8e9193cb6846f28"]/div/div/a')
 button1.click()
 time.sleep(1)
-tara = browser.find_element("xpath", '//*[@id="f75f5656e56542c78645cf2ff4fe6b05"]/div/b').text
+tara = browser.find_element("xpath", '//*[@id="d3e5c390370f45b196be78cb2f194969"]/div/b').text
+
 
 df.loc[1, current_date] = float(''.join([i for i in tara if i.isdigit()]))
 
